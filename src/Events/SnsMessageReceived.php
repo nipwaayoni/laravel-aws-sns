e658be34fbf7e22c95f4680834c1f46a816cfecd
Dirk Tepe <tepeds@miamioh.edu>
2021-04-29 11:35:45 -0400

    Add event classes

---
+++ src/Events/SnsMessageReceived.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace Nipwaayoni\SnsHandler\Events;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Nipwaayoni\SnsHandler\SnsMessage;
+
+
+class SnsMessageReceived
+{
+    use Dispatchable, InteractsWithSockets;
+
+    private $message;
+
+    public function __construct(SnsMessage $message)
+    {
+        $this->message = $message;
+    }
+
+    public function message(): SnsMessage
+    {
+        return $this->message;
+    }
+
+
+}
